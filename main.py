4db3dcb3991d0dd18dfba6af799616f1bba1c2cc
sol <osmanasa79@gmail.com>
2025-12-17 04:09:12 -0700

    added asteroids and asteroid field to screen and can see them moving on the screen

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
     dt = 0
     p_thing = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    a_thing = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
